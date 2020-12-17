b6d86a25df7dc39334d226e803af47c08188ad7a
Vovnenko Alexei <73690982+kestenbaum@users.noreply.github.com>
2020-12-17 10:59:45 +0200

    fixed v1.3

--- script/main.js
+++ script/main.js
@@ -1,2 +1,14 @@
 'use strict'
 
+let burgerWrap = document.querySelector('.burger_wrap'),
+    headerNav = document.querySelector('.header_nav')
+    // burgerActive = document.querySelector('.burger_active'),
+    // headerActive = document.querySelector('.header_nav_active')
+
+
+function clickBurger(){
+    burgerWrap.classList.toggle('burger_active')
+    headerNav.classList.toggle('header_nav_active')
+}
+
+burgerWrap.onclick = clickBurger
