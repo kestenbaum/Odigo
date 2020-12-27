0f023a47e7b0a3dac4626b5f97233b244d822725
Vovnenko Alexei <73690982+kestenbaum@users.noreply.github.com>
2020-12-27 02:59:20 +0200

    relise

--- script/main.js
+++ script/main.js
@@ -2,9 +2,6 @@
 
 let burgerWrap = document.querySelector('.burger_wrap'),
     headerNav = document.querySelector('.header_nav')
-    // burgerActive = document.querySelector('.burger_active'),
-    // headerActive = document.querySelector('.header_nav_active')
-
 
 function clickBurger(){
     burgerWrap.classList.toggle('burger_active')
@@ -12,3 +9,4 @@ function clickBurger(){
 }
 
 burgerWrap.onclick = clickBurger
+
